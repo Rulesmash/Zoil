9293cc441bc060557ec7bb9bf1451a062e07adf0
Aalif Mohammad RS <rulesmashpros@gmail.com>
2025-03-24 17:41:47 +0530

    added index and topic navigation

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask,redirect,render_template,url_for
+from flask import Flask, render_template
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("Mainpage.html")
+    return render_template('main.html')
 
-if __name__=='__main__':
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
     app.run(debug=True)
