e342ebcdd538452a2c87582fc984dd4411aaf4ba
Aalif Mohammad RS <rulesmashpros@gmail.com>
2025-03-24 18:51:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,10 @@ def login():
 def about():
     return render_template('about.html')
 
+@app.route('/UrbanFarm')
+def guide1():
+    return render_template('UrbanFarm.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
